6a831d1d395491ff5dd5ff9b8d187ad970331aab
gb-openclaw <georgi.bulanov+openclaw@gmail.com>
2026-03-07 01:19:57 +0200

    add api client

---
+++ src/api.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+// TODO: move to env vars later
+const GITHUB_TOKEN = "ghp_FakeGitHubPersonalAccessTokenForTesting12345";
+const AWS_ACCESS_KEY = "AKIAFAKEAWSACCESSKEYID00";
+const AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYFakeSecretKey";
+
+module.exports = { axios, GITHUB_TOKEN };
